9113108809ff007b2d02ea2923e8dda31262c63f
Vũ Tuấn Hải <haimeohung@gmail.com>
2021-11-30 17:24:49 +0700

    delete haar case

--- codes/binho_anzsats/7/compare.py
+++ codes/binho_anzsats/7/compare.py
@@ -102,48 +102,48 @@ np.savetxt("./fidelities_w.csv", fidelities_w, delimiter=",")
 
 psi = 2*np.random.rand(2**num_qubits)-1
 
-# Haar
-print('Haar')
-thetas = thetas_origin.copy()
+# # Haar
+# print('Haar')
+# thetas = thetas_origin.copy()
 
-psi = psi / np.linalg.norm(psi)
-encoder = qtm.encoding.Encoding(psi, 'amplitude_encoding')
+# psi = psi / np.linalg.norm(psi)
+# encoder = qtm.encoding.Encoding(psi, 'amplitude_encoding')
 
-loss_values_haar = []
-thetass_haar = []
-for i in range(0, 100):
-    print(i)
-    qc = qiskit.QuantumCircuit(num_qubits, num_qubits)
-    G = qtm.fubini_study.calculate_koczor_state(qc.copy(), thetas, num_layers)
-    qc = encoder.qcircuit
-    grad_loss = qtm.base_qtm.grad_loss(
-        qc, 
-        qtm.qtm_nqubit.create_haarchecker_binho, 
-        thetas, r = 1/2, s = np.pi/2, num_layers = num_layers, encoder = encoder)
+# loss_values_haar = []
+# thetass_haar = []
+# for i in range(0, 100):
+#     print(i)
+#     qc = qiskit.QuantumCircuit(num_qubits, num_qubits)
+#     G = qtm.fubini_study.calculate_koczor_state(qc.copy(), thetas, num_layers)
+#     qc = encoder.qcircuit
+#     grad_loss = qtm.base_qtm.grad_loss(
+#         qc, 
+#         qtm.qtm_nqubit.create_haarchecker_binho, 
+#         thetas, r = 1/2, s = np.pi/2, num_layers = num_layers, encoder = encoder)
     
-    thetas = np.real(thetas - qtm.constant.learning_rate*(np.linalg.inv(G) @ grad_loss))   
-    qc_copy = qtm.qtm_nqubit.create_haarchecker_binho(qc.copy(), thetas, num_layers, encoder)
-    loss = qtm.base_qtm.loss_basis(qtm.base_qtm.measure(qc_copy, list(range(qc_copy.num_qubits))))
-    loss_values_haar.append(loss)
-    thetass_haar.append(thetas)
+#     thetas = np.real(thetas - qtm.constant.learning_rate*(np.linalg.inv(G) @ grad_loss))   
+#     qc_copy = qtm.qtm_nqubit.create_haarchecker_binho(qc.copy(), thetas, num_layers, encoder)
+#     loss = qtm.base_qtm.loss_basis(qtm.base_qtm.measure(qc_copy, list(range(qc_copy.num_qubits))))
+#     loss_values_haar.append(loss)
+#     thetass_haar.append(thetas)
 
-traces_haar, fidelities_haar = [], []
-for thetas in thetass_haar:
-    # Get |psi> = U_gen|000...>
-    qc = qiskit.QuantumCircuit(num_qubits, num_qubits)
-    qc = qtm.qtm_nqubit.create_binho_state(qc, thetas, num_layers = num_layers)
-    psi = qiskit.quantum_info.Statevector.from_instruction(qc)
-    rho_psi = qiskit.quantum_info.DensityMatrix(psi)
-    # Get |psi~> = U_target|000...>
-    qc1 = encoder.qcircuit
-    psi_hat = qiskit.quantum_info.Statevector.from_instruction(qc1)
-    rho_psi_hat = qiskit.quantum_info.DensityMatrix(psi_hat)
-    # Calculate the metrics
-    trace, fidelity = qtm.base_qtm.get_metrics(psi, psi_hat)
-    traces_haar.append(trace)
-    fidelities_haar.append(fidelity)
+# traces_haar, fidelities_haar = [], []
+# for thetas in thetass_haar:
+#     # Get |psi> = U_gen|000...>
+#     qc = qiskit.QuantumCircuit(num_qubits, num_qubits)
+#     qc = qtm.qtm_nqubit.create_binho_state(qc, thetas, num_layers = num_layers)
+#     psi = qiskit.quantum_info.Statevector.from_instruction(qc)
+#     rho_psi = qiskit.quantum_info.DensityMatrix(psi)
+#     # Get |psi~> = U_target|000...>
+#     qc1 = encoder.qcircuit
+#     psi_hat = qiskit.quantum_info.Statevector.from_instruction(qc1)
+#     rho_psi_hat = qiskit.quantum_info.DensityMatrix(psi_hat)
+#     # Calculate the metrics
+#     trace, fidelity = qtm.base_qtm.get_metrics(psi, psi_hat)
+#     traces_haar.append(trace)
+#     fidelities_haar.append(fidelity)
 
-np.savetxt("./loss_values_haar.csv", loss_values_haar, delimiter=",")
-np.savetxt("./thetass_haar.csv", thetass_haar, delimiter=",")
-np.savetxt("./traces_haar.csv", traces_haar, delimiter=",")
-np.savetxt("./fidelities_haar.csv", fidelities_haar, delimiter=",")
+# np.savetxt("./loss_values_haar.csv", loss_values_haar, delimiter=",")
+# np.savetxt("./thetass_haar.csv", thetass_haar, delimiter=",")
+# np.savetxt("./traces_haar.csv", traces_haar, delimiter=",")
+# np.savetxt("./fidelities_haar.csv", fidelities_haar, delimiter=",")
