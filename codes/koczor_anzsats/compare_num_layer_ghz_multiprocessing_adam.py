e187d589d6a1fb37bcff7a9d8624760688232a3e
Vũ Tuấn Hải <haimeohung@gmail.com>
2021-12-16 21:03:03 +0700

    Run koczor 1-5 layer on 3 optimizers

---
+++ codes/koczor_anzsats/compare_num_layer_ghz_multiprocessing_adam.py
@@ -0,0 +1,83 @@
+import qiskit
+import numpy as np
+import sys
+import multiprocessing
+sys.path.insert(1, '../')
+import qtm.base_qtm, qtm.constant, qtm.qtm_nqubit, qtm.fubini_study, qtm.encoding
+import importlib
+importlib.reload(qtm.base_qtm)
+importlib.reload(qtm.constant)
+importlib.reload(qtm.qtm_1qubit)
+importlib.reload(qtm.qtm_nqubit)
+# Init parameters
+
+# For arbitrary initial state
+
+def run_ghz(num_layers, num_qubits):
+    # GHZ
+  
+    theta = np.pi/3
+    thetas = np.ones(num_qubits*num_layers*5)
+    qc = qiskit.QuantumCircuit(num_qubits, num_qubits)
+
+    loss_values_ghz = []
+    thetass_ghz = []
+    for i in range(0, 400):
+        # fubini_study for koczor_state is same for koczor state
+        if i % 20 == 0:
+            print('GHZ (' + str(num_layers) + ' layer): ', i)
+        # G = qtm.fubini_study.calculate_koczor_state(qc.copy(), thetas, num_layers)
+        grad_loss = qtm.base_qtm.grad_loss(
+            qc, 
+            qtm.qtm_nqubit.create_GHZchecker_koczor,
+            thetas, r = 1/2, s = np.pi/2, num_layers = num_layers, theta = theta)
+        # grad1 = np.real(np.linalg.inv(G) @ grad_loss)
+        if i == 0:
+            m, v = list(np.zeros(thetas.shape[0])), list(np.zeros(thetas.shape[0]))
+        thetas = qtm.base_qtm.adam(thetas, m, v, i, grad_loss)  
+        qc_copy = qtm.qtm_nqubit.create_GHZchecker_koczor(qc.copy(), thetas, num_layers, theta)  
+        loss = qtm.base_qtm.loss_basis(qtm.base_qtm.measure(qc_copy, list(range(qc_copy.num_qubits))))
+        loss_values_ghz.append(loss)
+        thetass_ghz.append(thetas)
+    traces_ghz, fidelities_ghz = [], []
+    for thetas in thetass_ghz:
+        # Get |psi> = U_gen|000...>
+        qc = qiskit.QuantumCircuit(num_qubits, num_qubits)
+        qc = qtm.qtm_nqubit.create_koczor_state(qc, thetas, num_layers = num_layers)
+        psi = qiskit.quantum_info.Statevector.from_instruction(qc)
+        rho_psi = qiskit.quantum_info.DensityMatrix(psi)
+        # Get |psi~> = U_target|000...>
+        qc1 = qiskit.QuantumCircuit(num_qubits, num_qubits)
+        qc1 = qtm.qtm_nqubit.create_ghz_state(qc1, theta = theta)
+        psi_hat = qiskit.quantum_info.Statevector.from_instruction(qc1)
+        rho_psi_hat = qiskit.quantum_info.DensityMatrix(psi_hat)
+        # Calculate the metrics
+        trace, fidelity = qtm.base_qtm.get_metrics(psi, psi_hat)
+        traces_ghz.append(trace)
+        fidelities_ghz.append(fidelity)
+        # Plot loss value in 100 steps
+    print('Writting ...')
+
+    np.savetxt("../../experiments/koczor_ansatz_15layer_adam/" + str(num_layers) + "/loss_values_ghz.csv", loss_values_ghz, delimiter=",")
+    np.savetxt("../../experiments/koczor_ansatz_15layer_adam/" + str(num_layers) + "/thetass_ghz.csv", thetass_ghz, delimiter=",")
+    np.savetxt("../../experiments/koczor_ansatz_15layer_adam/" + str(num_layers) + "/traces_ghz.csv", traces_ghz, delimiter=",")
+    np.savetxt("../../experiments/koczor_ansatz_15layer_adam/" + str(num_layers) + "/fidelities_ghz.csv", fidelities_ghz, delimiter=",")
+
+
+if __name__ == "__main__":
+    # creating thread
+    num_qubits = 5
+    num_layers = [1,2,3,4,5]
+   
+    t_ghz = []
+
+    for i in num_layers:
+        t_ghz.append(multiprocessing.Process(target = run_ghz, args=(i, num_qubits)))
+
+    for i in range(0, len(num_layers)):
+        t_ghz[i].start()
+
+    for i in range(0, len(num_layers)):
+        t_ghz[i].join()
+
+    print("Done!")
---
+++ codes/koczor_anzsats/compare_num_layer_haar_multiprocessing_qngadam.py
@@ -0,0 +1,78 @@
+import qiskit
+import numpy as np
+import sys
+import multiprocessing
+sys.path.insert(1, '../')
+import qtm.base_qtm, qtm.constant, qtm.qtm_nqubit, qtm.fubini_study, qtm.encoding
+
+
+def run_haar(num_layers, num_qubits):
+ 
+    psi = 2*np.random.rand(2**num_qubits)-1
+    # Haar
+    thetas = np.ones(num_qubits*num_layers*5)
+
+    psi = psi / np.linalg.norm(psi)
+    encoder = qtm.encoding.Encoding(psi, 'amplitude_encoding')
+
+    loss_values_haar = []
+    thetass_haar = []
+    for i in range(0, 400):
+        if i % 20 == 0:
+            print('Haar (' + str(num_layers) + ' layer): ', i)
+        qc = qiskit.QuantumCircuit(num_qubits, num_qubits)
+        G = qtm.fubini_study.calculate_koczor_state(qc.copy(), thetas, num_layers)
+        qc = encoder.qcircuit
+        grad_loss = qtm.base_qtm.grad_loss(
+            qc, 
+            qtm.qtm_nqubit.create_haarchecker_koczor, 
+            thetas, r = 1/2, s = np.pi/2, num_layers = num_layers, encoder = encoder)
+        grad1 = np.real(np.linalg.inv(G) @ grad_loss)
+        if i == 0:
+            m, v = list(np.zeros(thetas.shape[0])), list(np.zeros(thetas.shape[0]))
+        thetas = qtm.base_qtm.adam(thetas, m, v, i, grad1)    
+        qc_copy = qtm.qtm_nqubit.create_haarchecker_koczor(qc.copy(), thetas, num_layers, encoder)
+        loss = qtm.base_qtm.loss_basis(qtm.base_qtm.measure(qc_copy, list(range(qc_copy.num_qubits))))
+        loss_values_haar.append(loss)
+        thetass_haar.append(thetas)
+
+    traces_haar, fidelities_haar = [], []
+    for thetas in thetass_haar:
+        # Get |psi> = U_gen|000...>
+        qc = qiskit.QuantumCircuit(num_qubits, num_qubits)
+        qc = qtm.qtm_nqubit.create_koczor_state(qc, thetas, num_layers = num_layers)
+        psi = qiskit.quantum_info.Statevector.from_instruction(qc)
+        rho_psi = qiskit.quantum_info.DensityMatrix(psi)
+        # Get |psi~> = U_target|000...>
+        qc1 = encoder.qcircuit
+        psi_hat = qiskit.quantum_info.Statevector.from_instruction(qc1)
+        rho_psi_hat = qiskit.quantum_info.DensityMatrix(psi_hat)
+        # Calculate the metrics
+        trace, fidelity = qtm.base_qtm.get_metrics(psi, psi_hat)
+        traces_haar.append(trace)
+        fidelities_haar.append(fidelity)
+    print('Writting ... ' + str(num_layers))
+    np.savetxt("../../experiments/koczor_ansatz_15layer_qngadam/" + str(num_layers) + "/loss_values_haar.csv", loss_values_haar, delimiter=",")
+    np.savetxt("../../experiments/koczor_ansatz_15layer_qngadam/" + str(num_layers) + "/thetass_haar.csv", thetass_haar, delimiter=",")
+    np.savetxt("../../experiments/koczor_ansatz_15layer_qngadam/" + str(num_layers) + "/traces_haar.csv", traces_haar, delimiter=",")
+    np.savetxt("../../experiments/koczor_ansatz_15layer_qngadam/" + str(num_layers) + "/fidelities_haar.csv", fidelities_haar, delimiter=",")
+
+
+
+if __name__ == "__main__":
+    # creating thread
+    num_qubits = 5
+    num_layers = [1, 2, 3, 4, 5]
+   
+    t_haar = []
+
+    for i in num_layers:
+        t_haar.append(multiprocessing.Process(target = run_haar, args=(i, num_qubits)))
+
+    for i in range(0, len(num_layers)):
+        t_haar[i].start()
+
+    for i in range(0, len(num_layers)):
+        t_haar[i].join()
+
+    print("Done!")
