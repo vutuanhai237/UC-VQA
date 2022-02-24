3e71218e45bab48f79622ed8ecddec2eb2354e6c
Vũ Tuấn Hải <haimeohung@gmail.com>
2022-02-24 10:52:05 +0700

    Update compare_Wchain_qng

--- codes/tomography/compare_Wchain_qng.py
+++ codes/tomography/compare_Wchain_qng.py
@@ -15,47 +15,33 @@ importlib.reload(qtm.fubini_study)
 
 
 def run_wchain(num_layers, num_qubits):
-
-    thetas = np.ones(num_layers * num_qubits * 4)
-    psi = 2 * np.random.rand(2**num_qubits) - 1
+    thetas = np.ones(num_layers*num_qubits*4)
+    psi = 2*np.random.rand(2**num_qubits)-1
     psi = psi / np.linalg.norm(psi)
-    encoder = qtm.encoding.Encoding(psi, 'amplitude_encoding')
-
+    qc = qiskit.QuantumCircuit(num_qubits, num_qubits)
+    qc.initialize(psi)
     loss_values = []
     thetass = []
-
-    for i in range(0, 400):
-        if i % 20 == 0:
-            print('W_chain: (' + str(num_layers) + ',' + str(num_qubits) +
-                  '): ' + str(i))
-        qc = encoder.qcircuit
-        G = qtm.fubini_study.calculate_Wchain_state(qc.copy(), thetas,
-                                                    num_layers)
-
-        grad_loss = qtm.base.grad_loss(qc,
-                                       qtm.nqubit.create_Wchainchecker_haar,
-                                       thetas,
-                                       r=1 / 2,
-                                       s=np.pi / 2,
-                                       num_layers=num_layers)
-
-        thetas = np.real(thetas - qtm.constant.learning_rate *
-                         (np.linalg.pinv(G) @ grad_loss))
+    for i in range(0, 200):
+        if i % 10 == 0:
+            print('W_chain: ', i)
+        G = qtm.fubini_study.qng(qc.copy(), thetas, qtm.nqubit.create_Wchain_layerd_state, num_layers)
+        grad_loss = qtm.base.grad_loss(
+            qc, 
+            qtm.nqubit.create_Wchain_layerd_state,
+            thetas, r = 1/2, s = np.pi/2, num_layers = num_layers)
+        
+        thetas = np.real(thetas - qtm.constant.learning_rate*(np.linalg.inv(G) @ grad_loss))
         thetass.append(thetas.copy())
-        qc_copy = qtm.nqubit.create_Wchainchecker_haar(qc.copy(), thetas,
-                                                       num_layers)
-        loss = qtm.base.loss_basis(
-            qtm.base.measure(qc_copy, list(range(qc_copy.num_qubits))))
+        qc_copy = qtm.nqubit.create_Wchain_layerd_state(qc.copy(), thetas, num_layers)  
+        loss = qtm.base.loss_fubini_study(qtm.base.measure(qc_copy, list(range(qc_copy.num_qubits))))
         loss_values.append(loss)
+
 
     traces = []
     fidelities = []
 
     for thetas in thetass:
-        # Get |psi> = U_gen|000...>
-        qc1 = encoder.qcircuit
-        psi = qiskit.quantum_info.Statevector.from_instruction(qc1)
-        # Get |psi~> = U_target|000...>
         qc = qiskit.QuantumCircuit(num_qubits, num_qubits)
         qc = qtm.nqubit.create_Wchain_layerd_state(
             qc, thetas, num_layers=num_layers).inverse()
@@ -89,7 +75,7 @@ if __name__ == "__main__":
     # creating thread
 
     num_layers = [1, 2, 3, 4, 5]
-    num_qubits = [3, 4, 5]
+    num_qubits = [3, 4, 5, 6, 7, 8, 9, 10]
     t_wchains = []
 
     for i in num_layers:
