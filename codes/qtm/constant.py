0dedfbb0ea5a1c268f0967728955aaa6d0e4a5d8
Vũ Tuấn Hải <haimeohung@gmail.com>
2022-05-30 08:52:56 +0700

    Update optimizer

--- codes/qtm/constant.py
+++ codes/qtm/constant.py
@@ -3,7 +3,7 @@ import numpy as np
 
 # Training hyperparameter
 num_shots = 10000
-learning_rate = 0.02
+learning_rate = 0.01
 noise_prob = 0.04
 backend = qiskit.Aer.get_backend('qasm_simulator')
 
