f28e9b5d4ed63db9df79fbe18d37bfc519d37375
Vũ Tuấn Hải <43202025+vutuanhai237@users.noreply.github.com>
2023-04-09 13:45:45 +0700

    Update evolutional part

--- codes/qtm/qcompilation.py
+++ codes/qtm/qcompilation.py
@@ -20,9 +20,10 @@ class QuantumCompilation():
         self.fidelities = []
         self.traces = []
         self.kwargs = None
+        self.is_evolutional = False;
         return
 
-    def __init__(self, u: typing.Union[types.FunctionType, qiskit.QuantumCircuit], vdagger: typing.Union[types.FunctionType, qiskit.QuantumCircuit], optimizer: typing.Union[types.FunctionType, str], loss_func: typing.Union[types.FunctionType, str], thetas: np.ndarray = np.array([]), **kwargs):
+    def __init__(self, u: typing.Union[types.FunctionType, qiskit.QuantumCircuit], vdagger: typing.Union[types.FunctionType, qiskit.QuantumCircuit], optimizer: typing.Union[types.FunctionType, str], loss_func: typing.Union[types.FunctionType, str], thetas: np.ndarray = np.array([]), is_evolutional = False, **kwargs):
         """_summary_
 
         Args:
@@ -38,6 +39,7 @@ class QuantumCompilation():
         self.set_loss_func(loss_func)
         self.set_kwargs(**kwargs)
         self.set_thetas(thetas)
+        self.is_evolutional(is_evolutional)
         return
 
     def set_u(self, _u: typing.Union[types.FunctionType, qiskit.QuantumCircuit]):
@@ -152,9 +154,14 @@ class QuantumCompilation():
             - verbose (int, optional): 0, 1, or 2. Verbosity mode. 0 = silent, 1 = progress bar, 2 = one line per 10 steps. Verbose 1 is good for timing training time, verbose 2 if you want to log loss values to a file. Please install package tdqm if you want to use verbose 1. 
         
         """
-        self.thetass, self.loss_values = qtm.base.fit(
-            self.u, self.vdagger, self.thetas, num_steps, self.loss_func, self.optimizer, verbose, is_return_all_thetas=True, **self.kwargs)
+        if self.is_evolutional:
+            self.traces, self.fidelities = qtm.base.fit_evo(
+                self.u, self.vdagger, self.thetas, num_steps, self.loss_func, self.optimizer, verbose, is_return_all_thetas=True, **self.kwargs)
+        else:
+            self.thetass, self.loss_values = qtm.base.fit(
+                self.u, self.vdagger, self.thetas, num_steps, self.loss_func, self.optimizer, verbose, is_return_all_thetas=True, **self.kwargs)
         self.is_trained = True
+        
         if callable(self.u):
             self.traces, self.fidelities = qtm.utilities.calculate_state_preparation_metrics(self.u, self.vdagger, self.thetass, **self.kwargs)
         else:
