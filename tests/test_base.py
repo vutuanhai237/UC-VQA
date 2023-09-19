26e3f3792944d086ee4199dca27f70667f5dfa65
Vũ Tuấn Hải <43202025+vutuanhai237@users.noreply.github.com>
2023-09-19 22:45:42 +0700

    Move results into experiments folder

--- tests/test_base.py
+++ tests/test_base.py
@@ -19,6 +19,14 @@ def test_onequbit_tomography():
     )
     compiler.fit(num_steps = 100, verbose = 1)
     assert (np.min(compiler.loss_values) < 0.0001)
-
-
-test_onequbit_tomography()
+def test_nqubit_tomography():
+    num_qubits = 3
+    num_layers = 1
+    compiler = qtm.qcompilation.QuantumCompilation(
+        u = qtm.state.create_ghz_state(num_qubits).inverse(),
+        vdagger = qtm.ansatz.Wchain_ZXZlayer_ansatz(num_qubits, num_layers),
+        optimizer = 'adam',
+        loss_func='loss_fubini_study'
+    )
+    compiler.fit(num_steps=100, verbose = 1)
+    assert (np.min(compiler.loss_values) < 0.0001)
