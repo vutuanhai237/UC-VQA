8b1ef3b54ea9b685163762d3fa7d582c62a0455c
Vũ Tuấn Hải <43202025+vutuanhai237@users.noreply.github.com>
2023-10-03 12:34:29 +0700

    Update multi_process_script.py

--- codes/multi_process_script.py
+++ codes/multi_process_script.py
@@ -5,12 +5,22 @@ import qtm.qcompilation
 
 
 def f(num_qubits, num_layers):
-    print(num_qubits*num_layers)
+    optimizer = 'adam'
+    compiler = qtm.qcompilation.QuantumCompilation(
+        u = qtm.ansatz.g2gn(num_qubits, num_layers),
+        vdagger = qtm.state.create_AME_state(num_qubits).inverse(),
+        optimizer = optimizer,
+        loss_func = 'loss_fubini_study'
+    )
+    compiler.fit(num_steps = 100, verbose = 1)
+    qspobj = qtm.qsp.QuantumStatePreparation.load_from_compiler(
+    compiler = compiler, ansatz = qtm.ansatz.g2gn)
+    qspobj.save(state = 'ame', file_name='../experiments/qsp/')
     return 
 if __name__ == "__main__":
     # creating thread
     list_num_qubits = [3]
-    list_num_layers = [1,2,3,4,5]
+    list_num_layers = [1, 2]
    
     processes = [[0 for _ in range(len(list_num_layers))] for _ in range(len(list_num_qubits))]
     for i in range(len(list_num_qubits)):
