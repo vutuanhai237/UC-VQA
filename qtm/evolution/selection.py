188a929b24918b0dccdbee6d8bb9adf347420736
Vũ Tuấn Hải <43202025+vutuanhai237@users.noreply.github.com>
2023-10-21 15:08:09 +0700

    Split repo

--- qtm/evolution/selection.py
+++
@@ -1,7 +0,0 @@
-
-def elitist_selection(population, num_elitist = 0):
-    num_population = len(population)
-    if num_elitist == 0:
-        num_elitist = int(num_population/2)
-    population = sorted(population, key=lambda obj: obj.fitness)
-    return population
